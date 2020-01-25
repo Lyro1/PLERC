ece4dd05f3a426866b2df9eae51a1079aabe1cae
Louis Dupont <contact@lyro.fr>
2020-01-25 04:24:02 -0800

    [GPS] Added address to gps function

--- gps_module/address.py
+++ gps_module/address.py
@@ -5,3 +5,8 @@ def gps_from_address(address):
     location = geo_locator.geocode(address)
     return location.longitude, location.latitude
 
+def address_from_gps(latitude, longitude):
+    geo_locator = Nominatim()
+    location = geo_locator.reverse(longitude + ", " + latitude)
+    return location.address
+
