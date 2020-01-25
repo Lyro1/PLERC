ef2d064b1d3e1272dd16e12e89f9a727d641cf5e
Louis Dupont <contact@lyro.fr>
2020-01-25 04:47:57 -0800

    [TEST] Tested gps functions, all working

--- test.py
+++ test.py
@@ -1,28 +1,21 @@
 import networkx as nx
 import osmnx as ox
 import gps_module.address as gps
-import requests
-from matplotlib import pyplot as plt
-import matplotlib.cm as cm
-import matplotlib.colors as colors
-import sys
-import geopy
-from geopy.geocoders import Nominatim
 ox.config(use_cache=True, log_console=True)
 ox.__version__
 
 city_name = input("Quelle ville ?")
-if (city_name == "" or city_name == None):
+if city_name == "" or city_name is None:
     city_name = 'Biars-Sur-Cere'
 
 print("Chargement du graphe de " + city_name)
 
 origin_name = input("Adresse de départ :")
-if (origin_name == "" or origin_name == None):
+if origin_name == "" or origin_name is None:
     origin_name = "27 allee pierre loti biars-sur-cere"
 
 destination_name = input("Adresse d'arrivée :")
-if (destination_name == "" or destination_name == None):
+if destination_name == "" or destination_name is None:
     destination_name = "2 rue du stade biars-sur-cere"
 
 # get a graph for some city
@@ -45,13 +38,3 @@ destination_node = ox.get_nearest_node(G2, (destinationY, destinationX), method=
 route = nx.shortest_path(G2, origin_node, destination_node, weight='length')
 fig, ax = ox.plot_graph_route(G2, route, fig_height=20, fig_width=20)
 
-# get the nearest network node to each point
-
-#good_orig_node = ox.get_nearest_node(G2, (25.070661, 55.137911), method='euclidean')
-#dest_node = ox.get_nearest_node(G2, (25.079223, 55.146241), method='euclidean')
-
-
-
-# find the route between these nodes then plot it
-#route = nx.shortest_path(G2, good_orig_node, dest_node, weight='length')
-#fig, ax = ox.plot_graph_route(G2, route, fig_height=20, fig_width=20)
