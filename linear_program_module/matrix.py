28e053087ee28789d812de7ae19ebe6c4b14f6e1
Ricardo Ramos <ricardo.ramos@epita.fr>
2020-01-28 22:01:53 +0100

    added function for matrices

--- linear_program_module/matrix.py
+++ linear_program_module/matrix.py
@@ -3,6 +3,30 @@ def build_matrix_dictionary(nodes):
     dictionary = {}
     i = 0
     for node in nodes:
-        dictionary[i] = node
+        dictionary[node] = i
         i += 1
     return dictionary
+
+
+def build_different_matrices(nodes, vertices):
+    dict_nodes = build_matrix_dictionary(nodes)
+    dict_vertices = {}
+    N, V = len(nodes), len(vertices)
+    weights = []
+    M = [[0 for i in range(V)] for j in range(N)]
+    k = 0
+    for vertice in vertices:
+        i, j, w = dict_nodes[vertice[0]], dict_nodes[vertice[1]], vertice[2]
+        dict_vertices[(i, j)] = k
+        weights.append(w)
+        M[i][k] = 1
+        M[j][k] = -1
+        k += 1
+    return M, weights, dict_nodes, dict_vertices
+
+def build_constraints(i, j, N):
+    res = [0 for x in range(N)]
+    res[i] = 1
+    res[j] = -1
+    return res
+
