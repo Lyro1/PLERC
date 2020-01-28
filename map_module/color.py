005500b23d2356958e5f984a882bcfc66d79555c
Clotilde Saint-Denis <clotilde.saintdenis@gmail.com>
2020-01-28 22:49:41 +0100

    test

--- map_module/color.py
+++ map_module/color.py
@@ -1,4 +1,8 @@
 import osmnx
+import gps_module.address as gps
+import linear_program_module.matrix as lp
+osmnx.config(use_cache=True, log_console=True)
+osmnx.__version__
 
 def color_way(graph, way, beggin, end):
     """take a graph, the way (must be a list of noods), the origin and the last point
@@ -11,10 +15,14 @@ def color_way(graph, way, beggin, end):
 way = [3094515079, 3094515080, 3094515082, 3094515083]
 
 city_name = 'Biars-Sur-Cere'
-G2 = ox.graph_from_place(
+graph = osmnx.graph_from_place(
     city_name,
     simplify=False,
     retain_all=True,
     network_type='drive',
 )
 
+beggin = gps.gps_from_address("27 allee pierre loti biars-sur-cere")
+end = gps.gps_from_address("2 rue du stade biars-sur-cere")
+
+fig, ax = color_way(graph, way, beggin, end)
