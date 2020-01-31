a7cb810abed71b46923f6ecdfe6cb36304a8e59e
Louis Dupont <contact@lyro.fr>
2020-01-31 06:35:05 -0800

    [LP] Tried to simplify

--- test.py
+++ test.py
@@ -10,24 +10,27 @@ ox.__version__
 
 city_name = input("Quelle ville ?")
 if city_name == "" or city_name is None:
-    city_name = 'Biars-Sur-Cere'
+    city_name = 'Versailles France'
 
 print("Chargement du graphe de " + city_name)
 
 origin_name = input("Adresse de départ :")
 if origin_name == "" or origin_name is None:
-    origin_name = "27 allee pierre loti biars-sur-cere"
+    origin_name = "8 Rue des Missionnaires Versailles France"
 
 destination_name = input("Adresse d'arrivée :")
 if destination_name == "" or destination_name is None:
-    destination_name = "2 rue du stade biars-sur-cere"
+    destination_name = "66 Avenue de Paris Versailles France"
 
+print("Getting graph...")
 G2 = ox.graph_from_place(
         city_name,
         simplify=False,
         retain_all=True,
         network_type='drive',
     )
+
+print("Got graph.")
 while True:
     try:
         origin = gps.gps_from_address(origin_name)
@@ -39,6 +42,6 @@ while True:
 def test_one():
     assert test.compare_shortest_paths(G2,origin,destination)
 
-
+print("Solving route...")
 
 fig, ax = ox.plot_graph_route(G2, lp.get_shortest_path(G2, origin, destination), fig_height=20, fig_width=20)
