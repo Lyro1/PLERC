29053fea627aa824e7779c08af185f50f308d996
taillantou <pmtaillantou@gmail.com>
2020-01-31 14:33:07 +0100

    [TEST] Add test with pytest

--- test_path.py
+++ test_path.py
@@ -8,19 +8,11 @@ import pytest
 ox.config(use_cache=True, log_console=True)
 ox.__version__
 
-city_name = input("Quelle ville ?")
-if city_name == "" or city_name is None:
-    city_name = 'Biars-Sur-Cere'
+city_name = 'Biars-Sur-Cere'
 
-print("Chargement du graphe de " + city_name)
+origin_name = "27 allee pierre loti biars-sur-cere"
 
-origin_name = input("Adresse de départ :")
-if origin_name == "" or origin_name is None:
-    origin_name = "27 allee pierre loti biars-sur-cere"
-
-destination_name = input("Adresse d'arrivée :")
-if destination_name == "" or destination_name is None:
-    destination_name = "2 rue du stade biars-sur-cere"
+destination_name = "2 rue du stade biars-sur-cere"
 
 G2 = ox.graph_from_place(
         city_name,
@@ -40,5 +32,3 @@ def test_one():
     assert test.compare_shortest_paths(G2,origin,destination)
 
 
-
-fig, ax = ox.plot_graph_route(G2, lp.get_shortest_path(G2, origin, destination), fig_height=20, fig_width=20)
