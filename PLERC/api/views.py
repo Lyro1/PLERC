896c438536000a61c2fe06be71de4a54dbc3ca87
Etienne Mazet <etienne.mazet@epita.fr>
2020-02-04 20:23:57 +0100

    [UPDATE] Add version of location and test for you :p

--- PLERC/api/views.py
+++ PLERC/api/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
+import gps_module.address as gps
+import osmnx as ox
+
+
+graph = None
+city = None
 
 # Create your views here.
 def location(request, ville, adresse):
-    #test
-    raise Http404
+    if (city != ville):
+        load_graph(ville)
+    dep = gps.gps_from_address(adresse +" "+ ville)
+    loc = ox.get_nearest_node(graph, dep, method='euclidean')
+    return loc
 
 def path(request, ville, source, destination):
     raise Http404
