bd58b8a9b72b004383ca747f554920235dc2e306
Ricardo Ramos <ricardo.ramos@epita.fr>
2020-02-04 18:31:30 +0100

    [CORRECTION] Django : Corrected errors in views parameters

--- PLERC/api/views.py
+++ PLERC/api/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, Http404
 
 # Create your views here.
 def location(request, ville, adresse):
+    #test
     raise Http404
 
 def path(request, ville, source, destination):
