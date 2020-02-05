581cb31c8eb458167b10a7ae5618793f72fa602f
Louis Dupont <contact@lyro.fr>
2020-02-05 13:05:51 +0100

    Fixed map-module to load proper folium map

--- PLERC-Website/src/app/app.component.ts
+++ PLERC-Website/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 
-declare var ol: any;
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,24 +8,4 @@ declare var ol: any;
 
 export class AppComponent {
   title = 'PLERC-Website';
-
-  latitude = 48.8534;
-  longitude = 2.3488;
-
-  map: any;
-
-  ngOnInit() {
-    this.map = new ol.Map({
-      target: 'map',
-      layers: [
-        new ol.layer.Tile({
-          source: new ol.source.OSM()
-        })
-      ],
-      view: new ol.View({
-        center: ol.proj.fromLonLat([this.longitude, this.latitude]),
-        zoom: 8
-      })
-    });
-  }
 }
