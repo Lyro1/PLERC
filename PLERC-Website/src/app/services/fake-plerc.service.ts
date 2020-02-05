5807fd53e45b2813361a23b59c03aa5e9f404324
Louis Dupont <contact@lyro.fr>
2020-02-05 15:46:22 +0100

    Added services and fake API

---
+++ PLERC-Website/src/app/services/fake-plerc.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {AppConfigService} from './app-config.service';
+import {HttpClient} from '@angular/common/http';
+import {PathModel} from '../models/path.model';
+import {PathDataModel} from '../models/path-data.model';
+import {Observable, of} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FakePlercService {
+
+  constructor(private appConfigService: AppConfigService,
+              private http: HttpClient) {}
+
+  public isPointValid(town: string, location: string) {
+    return true;
+  }
+
+  public getPath(town: string, start: string, end: string): Observable<PathModel> {
+    return of(new PathModel());
+  }
+
+  public getPathData(town: string, start: string, end: string): Observable<PathDataModel> {
+    return of(new PathDataModel(2360, 48.83728));
+  }
+}
