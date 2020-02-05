37c3e7bb8caa01674d776557fa1f70aaa623d205
Louis Dupont <contact@lyro.fr>
2020-02-05 02:17:29 -0800

    Cleaned stats functions

--- map_module/color.py
+++ map_module/color.py
@@ -6,34 +6,3 @@ def color_way(graph, way):
     points as tuple (lat, lon)"""
     ox.plot_graph_route(graph, way)
 
-
-def length_way(list_of_edges):
-    length_total = 0
-    for edges in list_of_edges:
-        if "length" in edges[2]:
-            length_total += int(edges[2]["length"])
-        else:
-            length_total += 20
-    return length_total
-
-
-def speed_way(list_of_edges):
-    speed_total = 0
-    count = 0
-    for edges in list_of_edges:
-        if "maxspeed" in edges[2]:
-            speed_total += int(edges[2]["maxspeed"])
-        else:
-            speed_total += 50
-        count += 1
-    return speed_total / count
-
-
-def time_way(length, speed):
-    if speed == 0:
-        print("error")
-        return -1
-    if length == 0:
-        print("Vous etes arrivé")
-    return length/speed
-
---
+++ map_module/stats.py
@@ -0,0 +1,33 @@
+
+def length_way(list_of_edges):
+    length_total = 0
+    for edges in list_of_edges:
+        if "length" in edges[2]:
+            length_total += int(edges[2]["length"])
+        else:
+            length_total += 20
+    return length_total
+
+
+def speed_way(list_of_edges):
+    speed_total = 0
+    count = 0
+    for edges in list_of_edges:
+        if "maxspeed" in edges[2]:
+            speed_total += int(edges[2]["maxspeed"])
+        else:
+            speed_total += 50
+        count += 1
+    return speed_total / count
+
+
+def time_way(length, speed):
+    if speed == 0:
+        raise Exception('Speed can\'t be null')
+    return length/speed
+
+
+def get_path_stats(path):
+    length = length_way(path)
+    speed = speed_way(path)
+    return length, speed, time_way(length/1000, speed)
