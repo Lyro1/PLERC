8021541814e7ea2106fa03c26b92ea70dac1ba3d
Louis Dupont <contact@lyro.fr>
2020-02-12 19:58:12 +0100

    [API] Changed path color

--- map_module/save.py
+++ map_module/save.py
@@ -8,7 +8,7 @@ def save_path_to_png(graph, path, filename, width=20, height=20):
 
 
 def get_html_from_path(graph, path, weight=None, weight_realtime=None):
-    folium_map = ox.plot.plot_route_folium(graph, path, route_color='#42a5f5')
+    folium_map = ox.plot.plot_route_folium(graph, path, route_color='#ff0000')
     if weight_realtime is None or weight is None:
         lines = trafic.getColoredLines(graph, 20, 100)
     else:
