25d944dbe9b79ad073771be22e03d58dc9d108f3
Louis Dupont <contact@lyro.fr>
2020-02-12 21:00:20 +0100

    [ALGO] Fixed save to html

--- map_module/save.py
+++ map_module/save.py
@@ -7,13 +7,16 @@ def save_path_to_png(graph, path, filename, width=20, height=20):
     ox.plot.plot_graph(graph, path, show=False, filename=filename, fig_height=height, fig_width=width)
 
 
-def get_html_from_path(graph, path, weight=None, weight_realtime=None):
+def get_html_from_path(graph, path, traficEnabled=False, weight=None, weight_realtime=None):
+    print(graph)
+    print(path)
     folium_map = ox.plot.plot_route_folium(graph, path, route_color='#ff0000')
-    if weight_realtime is None or weight is None:
-        lines = trafic.getColoredLines(graph, 20, 100)
-    else:
-        lines = trafic.colorEdgesInRealTime(graph, weight, weight_realtime)
-    for line in lines:
-        folium_map.add_children(line)
+    if traficEnabled:
+        if weight_realtime is None or weight is None:
+            lines = trafic.getColoredLines(graph, 20, 100)
+        else:
+            lines = trafic.colorEdgesInRealTime(graph, weight, weight_realtime)
+        for line in lines:
+            folium_map.add_children(line)
     return folium_map.get_root().render()
 
